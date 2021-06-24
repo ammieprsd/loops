b515afcf2bb1991239e30a46393bce6c31d7e6fa
ammieprsd <ammieprsd@gmail.com>
2021-06-24 20:40:33 +0200

    opdracht3

--- loops.js
+++ loops.js
@@ -44,12 +44,12 @@ for (let i = 0; i < 4; i++){
 // ==========================================
 
 
-// for (let i = 0; i < 16; i++)
-//     if (i % 2 == 0 ){
-//         console.log(i + " is even")
-//     }else {
-//         console.log(i + " is oneven")
-//     }
+for (let i = 0; i < 16; i++)
+    if (i % 2 == 0 ){
+        console.log(i + " is even")
+    }else {
+        console.log(i + " is oneven")
+    }
 
 
 
