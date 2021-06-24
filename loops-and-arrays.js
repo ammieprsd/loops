77dd7d4336aca54938de752651a188d26701be9e
ammieprsd <ammieprsd@gmail.com>
2021-06-24 20:52:42 +0200

    opdracht1

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -5,6 +5,10 @@
 // ==========================================
 
 const names = ["Henk", "Piet", "Fred", "Joop"];
+for (let i = 0; i < names.length; i++){
+    console.log(names[i] + "je")
+}
+
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
